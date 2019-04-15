f3ed7e3333701adf5ed846b42f9a4de0f481a437
Noah Tutt <noahtutt@umich.edu>
2019-04-15 17:15:58 -0400

    best model analysis

--- project.R
+++ project.R
@@ -1,14 +1,21 @@
 #!/usr/bin/R -q --slave -f
 
+library(corrplot)
+library(HH)
+library(leaps)
+library(car)
+
+set.seed(11)
+
 mlb2016 = read.csv("mlb_team_data_2016.csv")
 mlb2017 = read.csv("mlb_team_data_2017.csv")
 mlb2018 = read.csv("mlb_team_data_2018.csv")
 
+
 mlb = rbind(mlb2016, mlb2017, mlb2018)
 
 trimmed_features = c(
-	"name",
-	"abbreviation",
+	"win_percentage",
 	"average_batter_age",
 	"average_pitcher_age",
 	"batting_average",
@@ -19,7 +26,6 @@ trimmed_features = c(
 	"runs",
 	"runs_against",
 	"strikeouts_per_nine_innings",
-	"win_percentage",
 	"stolen_bases",
 	"strikeouts_per_base_on_balls",
 	"whip",
@@ -39,5 +45,49 @@ for (feature in trimmed_features) {
 	. = dev.off()
 }
 
+# suggests that some variables should be removed
+png("correlation_plot.png", width = 720, height = 720)
+corrplot(cor(mlb_trimmed[-1]))
+. = dev.off()
+cor(mlb_trimmed$win_percentage, mlb_trimmed)
 
-	
+# plot weak variables
+stolen_bases_model = lm(win_percentage ~ stolen_bases, data = mlb_trimmed)
+summary(stolen_bases_model)
+
+average_batter_age_model = lm(win_percentage ~ average_batter_age, data = mlb_trimmed)
+summary(average_batter_age_model)
+
+test_observations = sample(90, 60, replace = FALSE)
+mlb_train = mlb_trimmed[test_observations, ]
+mlb_test = mlb_trimmed[-test_observations, ]
+
+# best is average_batter_age-hits_allowed-runs-runs_against-saves-times_struck_out
+mlb_all = regsubsets(win_percentage ~ . - win_percentage, data = mlb_train)
+summaryHH(mlb_all)
+
+formula_p7 = "win_percentage ~ average_batter_age+average_pitcher_age+earned_runs_against+hits_allowed+runs+saves+times_struck_out"
+formula_p4 = "win_percentage ~ earned_runs_against+runs+saves+times_struck_out"
+
+mlb_p7 = lm(as.formula(formula_p7), data = mlb_train)
+summary(mlb_p7)
+
+mlb_p4 = lm(as.formula(formula_p4), data = mlb_train)
+summary(mlb_p4)
+
+png(filename = "avplots/p7.png", width = 720, height = 720)
+avPlots(mlb_p7)
+. = dev.off()
+
+png(filename = "avplots/p4.png", width = 720, height = 720)
+avPlots(mlb_p4)
+. = dev.off()
+
+png(filename = "influence/p7.png", width = 720, height = 720)
+influencePlot(mlb_p4)
+. = dev.off()
+
+png(filename = "influence/p4.png", width = 720, height = 720)
+influencePlot(mlb_p4)
+. = dev.off()
+
